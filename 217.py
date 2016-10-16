932ae9fc049c0587572820b8c717a443dcc05575
madinhos <madinhos@gmail.com>
2016-10-16 16:31:51 +0200

    Delete 217.py

--- 217.py
+++
@@ -1,5 +0,0 @@
-line = "Lorem ipsum dolor sit amet consectetur adipiscing elit"
-print line
-print "Sortowanie aflabetyczne", sorted(line.split(), key=lambda v: (v.upper(), v[0].islower()))
-print "Sortowanie po dlugosci", sorted(line.split(), key=len)
-raw_input()
