df060ddd295b64bda9e56709b466c2298b21eb94
madinhos <madinhos@gmail.com>
2016-10-16 16:31:24 +0200

    Delete 214.py

--- 214.py
+++
@@ -1,7 +0,0 @@
-line = "Lorem ipsum dolor sit amet consectetur adipiscing elit"
-print line
-a = line.split()
-print "najdluzszy wyraz -> ", max(a, key=len)
-print "dlugosc -> ", len(max(a, key=len))
-
-raw_input()
